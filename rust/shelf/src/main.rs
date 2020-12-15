8be39d3e36825c89541ad0022050ed83b3d070eb
Cameron Durham <camerondurham@users.noreply.github.com>
2020-12-15 02:45:33 -0800

    Add rust starter code

---
+++ rust/shelf/src/main.rs
@@ -0,0 +1,26 @@
+extern crate elf;
+
+use std::path::PathBuf;
+
+fn main() {
+    let path = PathBuf::from("tests/gdb/gdb");
+    let file = match elf::File::open_path(&path) {
+        Ok(f) => f,
+        Err(e) => panic!("Error: {:?}", e),
+    };
+
+    let section = match file.get_section(".interp") {
+        Some(s) => s,
+        None => panic!("Failed to look up .interp section"),
+    };
+
+    println!("{:?}", section.data);
+
+    // TODO: strip trailing null character from string: more elegantly?
+    if section.data.len() > 1 {
+        let as_str = String::from_utf8_lossy(&section.data[0..section.data.len() - 1]);
+        println!("as_str: {:?}", as_str);
+
+        // TODO: check if file path is valid
+    }
+}
