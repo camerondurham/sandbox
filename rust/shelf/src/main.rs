bce5dc1b70ebb14425b65cfac1f4e7558b694c15
Cameron Durham <cdurham@usc.edu>
2020-12-16 02:02:33 -0800

    elf

--- rust/shelf/src/main.rs
+++ rust/shelf/src/main.rs
@@ -1,6 +1,8 @@
 extern crate elf;
 
 use std::path::PathBuf;
+use std::fs::Metadata;
+use std::fs;
 
 fn main() {
     let path = PathBuf::from("tests/gdb/gdb");
@@ -19,8 +21,10 @@ fn main() {
     // TODO: strip trailing null character from string: more elegantly?
     if section.data.len() > 1 {
         let as_str = String::from_utf8_lossy(&section.data[0..section.data.len() - 1]);
-        println!("as_str: {:?}", as_str);
+        println!("as_str: {:?}", &as_str);
 
         // TODO: check if file path is valid
+        let metadata = fs::metadata(String::from(as_str)).unwrap();
+        println!("permissions: {:?}", metadata.permissions())
     }
 }
