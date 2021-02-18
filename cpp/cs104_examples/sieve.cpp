2b1a02877b84383bfdad0beeffe9c67657b2f46a
Cameron Durham <cdurham@usc.edu>
2021-02-17 22:28:24 -0800

    Functional Sieve

--- cpp/cs104_examples/sieve.cpp
+++ cpp/cs104_examples/sieve.cpp
@@ -2,20 +2,23 @@
 #include<vector>
 #include<string>
 #include<numeric>
+#include<algorithm>
 int main() {
-  std::vector<int> v;
-  for(int i = 2; i < 1000000; i++){
-      bool prime = true;
-      for(int j : v){
-        if (i % j == 0){
-          prime = false;
-          break;
-        }
-        if (j * j > i) break;
+  std::vector<int> v(10000);
+  std::vector<int> p;
+  std::iota(v.begin(), v.end(), 2);
+  std::for_each(v.begin(), v.end(), [&p](int i){
+    bool prime = true;
+    for(int j : p){
+      if(i % j == 0){
+        prime = false;
+        break;
       }
-      if(prime) v.push_back(i);
-  }
-  std::cout << std::accumulate(std::next(v.begin()), v.end(), std::to_string(v.at(0)), [](std::string ss,int i){
+      if(j * j > i) break;
+    }
+    if(prime) p.push_back(i);
+  });
+  std::cout << std::accumulate(std::next(p.begin()), p.end(), std::to_string(p.at(0)), [](std::string ss,int i){
   return std::move(ss)+','+std::to_string(i);
   });
 }
