8c10340358dd79cb808c89311a8b5576068300af
Cameron Durham <cdurham@usc.edu>
2021-02-19 23:21:33 -0800

    Test commit

--- cpp/scratch.cpp
+++ cpp/scratch.cpp
@@ -8,10 +8,8 @@ void printFile(std::string);
 int main() {
   // std::stringstream ss;
   // ss << 100 << ' ' << 200;
-
   // int foo, bar;
   // ss >> foo >> bar;
-
   // std::cout << "foo: " << foo << '\n';
   // std::cout << "bar: " << bar << '\n';
   std::cout << "hello world" << std::endl;
