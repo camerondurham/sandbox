7b3ad72a49127790a9f981fce5d9bcbf6d0d4182
Cameron Durham <cdurham@usc.edu>
2021-02-24 12:02:08 +0000

    Add untested implementation

--- cpp/cs104_examples/midterm-review/keyboard/keyboard.cpp
+++ cpp/cs104_examples/midterm-review/keyboard/keyboard.cpp
@@ -1,9 +1,31 @@
 #include "keyboard.h"
 #include <map>
+#include <vector>
+#include <functional>
+#include <utility>
+#include <numeric>
 
-using namespace std;
 
-int calculateTime(string keyboard, string word) {
-	int sum = 0;
-	return sum;
+int calculateTime(std::string keyboard, std::string word) { 
+	int sum(0); 
+	std::vector<int> permutation(26);
+
+	for(size_t i = 0; i < keyboard.size(); ++i)
+	{
+		permutation[static_cast<int>(keyboard[i] - 'a')] = i;
+	}
+	for(size_t i = 0; i < word.size(); ++i)
+	{
+		if(i == 0)
+		{
+			sum += permutation[static_cast<size_t>(word[i]-'a')];
+		}
+		else
+		{
+			int dist =  permutation[static_cast<size_t>(word[i] - 'a')] + permutation[static_cast<size_t>(word[i-1])];
+			sum += (dist < 0 ? -dist : dist);
+		}
+	}
+
+	return sum; 
 }
