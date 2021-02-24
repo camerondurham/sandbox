37b3757f35648c2e0316c9c7578ef32e45bca350
Cameron Durham <cdurham@usc.edu>
2021-02-24 09:38:38 -0800

    Add keyboard solution

--- cpp/cs104_examples/midterm-review/keyboard/keyboard.cpp
+++ cpp/cs104_examples/midterm-review/keyboard/keyboard.cpp
@@ -1,31 +1,36 @@
 #include "keyboard.h"
-#include <map>
 #include <vector>
-#include <functional>
-#include <utility>
-#include <numeric>
-
-
-int calculateTime(std::string keyboard, std::string word) { 
-	int sum(0); 
-	std::vector<int> permutation(26);
+#include <map>
+int calculateTime(std::string keyboard, std::string word) {
+	int sum(0);
+	std::vector<int> permutation(keyboard.size());
 
 	for(size_t i = 0; i < keyboard.size(); ++i)
 	{
 		permutation[static_cast<int>(keyboard[i] - 'a')] = i;
 	}
-	for(size_t i = 0; i < word.size(); ++i)
+
+	sum = permutation[static_cast<size_t>(word[0] - 'a')];
+
+	for (size_t i = 1; i < word.size(); ++i)
 	{
-		if(i == 0)
-		{
-			sum += permutation[static_cast<size_t>(word[i]-'a')];
-		}
-		else
-		{
-			int dist =  permutation[static_cast<size_t>(word[i] - 'a')] + permutation[static_cast<size_t>(word[i-1])];
-			sum += (dist < 0 ? -dist : dist);
-		}
+		sum += std::abs(permutation[static_cast<size_t>(word[i] - 'a')] - permutation[static_cast<size_t>(word[i - 1] - 'a')]);
 	}
 
-	return sum; 
+	return sum;
 }
+
+
+
+// int calculateTime(std::string keyboard, std::string word) {
+// 	int sum = 0;
+// 	std::map<char,int> letters;
+// 	for(size_t i = 0; i < keyboard.size(); i++) {
+// 		letters[keyboard[i]] = i;
+// 	}
+// 	sum += letters[word[0]];
+// 	for(size_t i = 1; i < word.size(); i++) {
+// 		sum += abs(letters[word[i]] - letters[word[i-1]]);
+// 	}
+// 	return sum;
+// }
