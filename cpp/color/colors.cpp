02ef14c0cddc34834f6821183d3199135a33a7ff
Cameron Durham <cdurham@usc.edu>
2021-03-12 13:08:26 +0000

    Add more colors, background colors

--- cpp/color/colors.cpp
+++ cpp/color/colors.cpp
@@ -1,31 +1,38 @@
 #include<iostream>
 #include<string>
 #include<map>
+#include "colors.h"
 
-const std::string COLOR_CYAN = "\u001b[36;1m";
-const std::string COLOR_MAGENTA = "\u001b[35;1m";
-const std::string COLOR_BLUE = "\u001b[34;1m";
-const std::string COLOR_YELLOW = "\u001b[33;1m";
-const std::string COLOR_GREEN = "\u001b[32;1m";
-const std::string COLOR_RED = "\u001b[31;1m";
-const std::string COLOR_PINK = "\u001b[38;5;206m";
-const std::string COLOR_RESET = "\u001b[0m";
-
-int main() {
+int main(int argc, char** argv) {
   std::map<std::string, std::string> m = {
-	  {"CYAN", COLOR_CYAN},
-	  {"MAGENTA", COLOR_MAGENTA},
-	  {"BLUE", COLOR_BLUE},
-	  {"YELLOW", COLOR_YELLOW},
-	  {"GREEN", COLOR_GREEN},
-	  {"RED", COLOR_RED},
-	  {"PINK", COLOR_PINK}
+	  {"CYAN", Color::CYAN},
+	  {"MAGENTA", Color::MAGENTA},
+	  {"BLUE", Color::BLUE},
+	  {"YELLOW", Color::YELLOW},
+	  {"GREEN", Color::GREEN},
+	  {"RED", Color::RED},
+	  {"PINK", Color::PINK},
+
+	  {"BG_CYAN", BgColor::CYAN},
+	  {"BG_MAGENTA", BgColor::MAGENTA},
+	  {"BG_BLUE", BgColor::BLUE},
+	  {"BG_YELLOW", BgColor::YELLOW},
+	  {"BG_GREEN", BgColor::GREEN},
+	  {"BG_RED", BgColor::RED},
+	  {"BG_PINK", BgColor::PINK}
   };
 
   for (auto it = m.begin(); it != m.end(); ++it)
   {
-	  std::cout << it->second << " " << it->first << COLOR_RESET << "\n";
+	  std::cout << it->second << " " << it->first << "     " << Color::RESET << "\n";
   }
+  
+  // Trans Pride!
 
-  // std::cout << COLOR_GREEN << " green " << COLOR_RED << " red " << COLOR_MAGENTA << " magenta " << COLOR_RESET << " reset\n";
+  std::cout << BgColor::BLUE << "                         \n";
+  std::cout << BgColor::PINK << "                         \n";
+  std::cout << BgColor::YELLOW << "                         \n";
+  std::cout << BgColor::PINK << "                         \n";
+  std::cout << BgColor::BLUE << "                         \n";
+  std::cout << BgColor::RESET;
 }
---
+++ cpp/color/colors.h
@@ -0,0 +1,43 @@
+#include<string>
+
+struct Color {
+	static const std::string CYAN;
+	static const std::string MAGENTA;
+	static const std::string BLUE;
+	static const std::string YELLOW;
+	static const std::string GREEN;
+	static const std::string RED;
+	static const std::string PINK;
+	static const std::string RESET;
+};
+
+const std::string Color::CYAN =  "\u001b[36;1m";
+const std::string Color::MAGENTA = "\u001b[35;1m";
+const std::string Color::BLUE = "\u001b[34;1m";
+const std::string Color::YELLOW = "\u001b[33;1m";
+const std::string Color::GREEN = "\u001b[32;1m";
+const std::string Color::RED = "\u001b[31;1m";
+const std::string Color::PINK = "\u001b[38;5;206m";
+const std::string Color::RESET = "\u001b[0m";
+
+struct BgColor {
+	static const std::string CYAN;
+	static const std::string MAGENTA;
+	static const std::string BLUE;
+	static const std::string YELLOW;
+	static const std::string GREEN;
+	static const std::string RED;
+	static const std::string PINK;
+	static const std::string RESET;
+};
+
+
+const std::string BgColor::CYAN = "\u001b[48;5;14m";
+const std::string BgColor::MAGENTA = "\u001b[48;5;13m";
+const std::string BgColor::BLUE = "\u001b[48;5;27m";
+const std::string BgColor::YELLOW = "\u001b[48;5;11m";
+const std::string BgColor::GREEN = "\u001b[48;5;10m";
+const std::string BgColor::RED = "\u001b[48;5;9m";
+const std::string BgColor::PINK = "\u001b[48;5;207m";
+const std::string BgColor::RESET = "\u001b[0m";
+
