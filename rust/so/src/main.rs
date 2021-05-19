11e203bfdf13807a6138c911e5f8884cb4ecbee5
Cameron Durham <cdurham@usc.edu>
2021-05-19 01:01:15 -0700

    SO API working

--- rust/so/src/main.rs
+++ rust/so/src/main.rs
@@ -1,30 +1,26 @@
-use reqwest::header::{CONTENT_ENCODING, CONTENT_TYPE};
+use reqwest::header::{ACCEPT_ENCODING};
+use serde_json;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     
-    let resp = reqwest::Client::new()
-    .get("https://api.stackexchange.com/2.2/questions/featured")
-    .header(CONTENT_TYPE, "application/json")
-    .header(CONTENT_ENCODING, "utf-8")
+    let client = reqwest::Client::builder()
+    .gzip(true)
+    .build()?;
+    let resp = client
+    .get("https://api.stackexchange.com/2.2/questions/featured/")
+    .header(ACCEPT_ENCODING, "gzip")
     .query(&[("order", "desc"), ("sort", "creation"), ("site", "stackoverflow")])
     .send()
     .await?;
 
-    // let resp = reqwest::Client::new()
-    // .get("https://api.stackexchange.com/2.2/questions/unanswered/my-tags")
-    // .query(&[("fromdate", "1621123200"), ("order", "desc"), ("sort", "creation"), ("site", "stackoverflow")])
-    // .header("access_token", "VEZu*G7zmmbsewpD7g8Nqg))")
-    // .send()
-    // .await?
-    //     .json::<HashMap<String, HashMap<String,String>>>()
-    // .await?;
-
-
     // let resp = reqwest::get("https://httpbin.org/ip")
+
     println!("Status: {}", resp.status());
-    println!("{:#?}", resp.json().await?);
+    println!("{:#?}", resp);
+    let parsed: serde_json::Value = serde_json::from_str(&resp.text().await?)?;
+    println!("Parsed json: {:#?}", parsed);
     Ok(())
 }
