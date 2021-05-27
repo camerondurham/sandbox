0e779276ce33606e06eebb00a9f96cf0720d5ae3
Cameron Durham <cdurham@usc.edu>
2021-05-26 23:46:48 -0700

    Vecs example

--- rust/book/8_collections/vecs.rs
+++ rust/book/8_collections/vecs.rs
@@ -26,4 +26,14 @@ fn main() {
     for i in &v {
         println!("{}", i);
     }
+
+    let v = vec![1,2,3,4,5];
+
+    let third:  &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
 }
