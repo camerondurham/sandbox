62f24031b1e46b57857d6c8d94581f641311afc3
Cameron Durham <cdurham@usc.edu>
2021-06-02 19:40:15 -0700

    Begin error chapter

---
+++ rust/book/9_errors/error_tests/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn crash_and_burn() {
+    panic!("crash and burn")
+}
+fn main() {
+    println!("Hello, world!");
+    let v = vec![1,2,3];
+    // causes a panic/OOB error
+    // v[99];
+    // crash_and_burn();
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // more sophisticated way to write this 🦀
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
