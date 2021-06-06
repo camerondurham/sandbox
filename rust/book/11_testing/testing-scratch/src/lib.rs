3e99abe4a277fb9f25deb059c33e09cb9cd8f08e
Cameron Durham <cdurham@usc.edu>
2021-06-05 22:05:49 -0700

    Add unit and integration test section

---
+++ rust/book/11_testing/testing-scratch/src/lib.rs
@@ -0,0 +1,104 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+    // String::from("Hello!")
+}
+
+#[cfg(test)]
+mod tests {
+
+    // #[test]
+    // fn exploration() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail!");
+    // }
+
+    // ---
+
+    // since tests module is an inner module, we need to bring
+    // the outer module into scope here:
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    // testing equality with assert_eq! and assert_ne! macros
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    // Using Result<T, E> in Tests
+
+    // enables use of the ? operator in the body of tests
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
