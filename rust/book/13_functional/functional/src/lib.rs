69a85660804ce61940c795c39b485fa3ff64fbe9
Cameron Durham <cdurham@usc.edu>
2021-06-06 01:20:36 -0700

    Add first section of chapter 13

---
+++ rust/book/13_functional/functional/src/lib.rs
@@ -0,0 +1,88 @@
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
+
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     println!("calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
+
+pub struct Cacher<T>
+    where
+// in c++?
+// std::function<unsigned int, int>
+        T: Fn(u32) -> u32,
+{
+    calculation: T,
+    memo: HashMap<u32,u32>,
+}
+
+impl<T> Cacher<T>
+    where
+        T: Fn(u32) -> u32,
+{
+    pub fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            memo: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.memo.get(&arg) {
+            Some(v) => v.clone(),
+            None => {
+                let v = (self.calculation)(arg);
+                self.memo.insert(arg,v);
+                v.clone()
+            }
+        }
+    }
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
+    }
+}
+
+
---
+++ rust/book/13_functional/functional/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let simulated_user_specified_value = 25;
+    let simulated_random_number = 3;
+
+    functional::generate_workout(simulated_user_specified_value, simulated_random_number);
+}
