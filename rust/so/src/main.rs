62d63b99eb29897150c72ed711a9e29afad20e86
Cameron Durham <cdurham@usc.edu>
2021-06-16 23:39:55 -0700

    Add new Stack Overflow API question

--- rust/so/src/main.rs
+++ rust/so/src/main.rs
@@ -1,6 +1,8 @@
 use reqwest::{Response, header::{ACCEPT_ENCODING}};
 use serde_json;
 
+const API_V2_2_BASE_URL : &str = "https://api.stackexchange.com/2.2";
+
 fn auth() {
     // TODO: implement auth flow specified in https://api.stackexchange.com/docs/authentication
 }
@@ -19,6 +21,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .get("https://api.stackexchange.com/2.2/questions/featured/")
     .header(ACCEPT_ENCODING, "gzip")
     .query(&[("order", "desc"), ("sort", "creation"), ("site", "stackoverflow")])
+    .send()
+    .await?;
+
+    log_http_response(&resp);
+
+    let parsed: serde_json::Value = serde_json::from_str(&resp.text().await?)?;
+    println!("Parsed json: {:#?}", parsed);
+
+
+    // https://api.stackexchange.com/docs/answers-on-users#pagesize=100&order=desc&sort=activity&ids=4676641&filter=default&site=stackoverflow&run=true
+    // path: /2.2/users/4676641/answers?pagesize=100&order=desc&sort=activity&site=stackoverflow
+
+    let user_id = "4676641";
+    let resp = client
+    .get(format!("{}/{}/{}/{}", API_V2_2_BASE_URL, "users",user_id, "answers"))
+    .header(ACCEPT_ENCODING,"gzip")
+    .query(&[("order", "desc"), ("sort","activity"),("site","stackoverflow")])
     .send()
     .await?;
 
